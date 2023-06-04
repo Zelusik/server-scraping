92cda99d35a8d3527686741364d22d2706bbb219
Wo-ogie <siwall0105@gmail.com>
2023-06-04 16:44:41 +0900

    docs: 장소 정보 scraping API 명세 작성

--- src/main/java/com/zelusik/scraping/controller/PlaceScrapingController.java
+++ src/main/java/com/zelusik/scraping/controller/PlaceScrapingController.java
@@ -3,6 +3,8 @@ package com.zelusik.scraping.controller;
 import com.zelusik.scraping.dto.place.BusinessHoursDto;
 import com.zelusik.scraping.dto.place.PlaceInfoResponse;
 import com.zelusik.scraping.service.PlaceScrapingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 
+@Tag(name = "장소 Scraping")
 @RequiredArgsConstructor
 @RequestMapping("/places/scraping")
 @RestController
@@ -26,6 +29,8 @@ public class PlaceScrapingController {
     private final PlaceScrapingService placeScrapingService;
     private final ChromeOptions createOptions;
 
+    @Operation(summary = "Kakao 장소 정보 scraping",
+            description = "<p>요청 시 전달받은 <code>kakaoPid</code>를 이용해 kakao map 상세 페이지에서 영업시간, 휴무일, 홈페이지 url을 추출해 응답한다.")
     @GetMapping
     public PlaceInfoResponse getKakaoPlaceInfo(@RequestParam String kakaoPid) {
         ChromeDriver driver = new ChromeDriver(createOptions);
