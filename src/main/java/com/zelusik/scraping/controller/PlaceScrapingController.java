9403b04a0e64765bd70ee9e3c252210be48a7ed7
Wo-ogie <siwall0105@gmail.com>
2023-06-04 16:42:03 +0900

    refactor: 장소 정보 조회 API의 endpoint 변경

--- src/main/java/com/zelusik/scraping/controller/PlaceScrapingController.java
+++ src/main/java/com/zelusik/scraping/controller/PlaceScrapingController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Duration;
 
 @RequiredArgsConstructor
-@RequestMapping("/places/scrap")
+@RequestMapping("/places/scraping")
 @RestController
 public class PlaceScrapingController {
 
